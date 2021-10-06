ca1c74f0ced5fa9ad429231aec5641c1de07cc88
Felipe Rocha de Lucena - 40259869813 <felipe.lucena@anima.intranet>
2021-10-06 18:25:30 -0300

    Config log

--- src/app.js
+++ src/app.js
@@ -2,9 +2,27 @@ const app = require("express")();
 const consign = require("consign");
 const knex = require("knex");
 const knexfile = require("../knexfile");
+const winston = require("winston");
+const uuid = require("uuidv4");
 
-//TODO criar chaveamento dinâmico
-app.db = knex(knexfile.test);
+app.db = knex(knexfile[process.env.NODE_ENV]);
+
+app.log = winston.createLogger({
+  level: "debug",
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.json({ space: 1 }),
+    }),
+    new winston.transports.File({
+      filename: "logs/error.log",
+      level: "warn",
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json({ space: 1 })
+      ),
+    }),
+  ],
+});
 
 consign({ cwd: "src", verbose: false })
   .include("./config/passport.js")
@@ -19,14 +37,17 @@ app.get("/", (req, res) => {
 });
 
 app.use((err, req, res, next) => {
+  const id = uuid();
   const { name, message } = err;
   if (name === "ValidationError") {
+    app.log.error({ id, name, message });
     res.status(400).json({ error: message });
   } else if (name === "RecursoIndevidoError") {
     res.status(403).json({ error: message });
   } else {
-    console.log(message);
-    res.status(500).json({ name, message });
+    const id = uuid();
+    app.log.error({ id, name, message });
+    res.status(500).json({ error: "Falha interna" });
   }
   next();
 });
--- src/config/middleware.js
+++ src/config/middleware.js
@@ -1,8 +1,12 @@
 const bodyParser = require("body-parser");
 // const knexLogger = require("knex-logger");
 
+// const cors = require("cors");
+
 module.exports = (app) => {
   app.use(bodyParser.json());
+
+  // app.use(cor({ origin: "*" }));
 };
 
 // app.use(knexLogger(app.db));
