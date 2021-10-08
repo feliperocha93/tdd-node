5ee1b43a4ee7ec9afacbab031bb5a0ad6ee174dd
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2021-10-08 19:07:07 -0300

    Add production database

--- knexfile.js
+++ knexfile.js
@@ -17,12 +17,12 @@ module.exports = {
   },
   prod: {
     client: "pg",
-    version: "9.6",
+    version: "",
     connection: {
-      host: "localhost",
-      user: "postgres",
-      password: "postgres",
-      database: "seubarriga",
+      host: "ec2-54-172-169-87.compute-1.amazonaws.com",
+      user: "qvfznrclbrcgst",
+      password: "171152868d023e3c9e50c7812a5c4374226da9fb618b9adab79cf69c571a769f",
+      database: "d2kijqt9cmg2f5",
     },
     migrations: {
       directory: "src/migrations",
